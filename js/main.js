bf63c0a512497669bab29068c9712853f9053cc4
Ailie McCorkindale <ailiem134@hotmail.com>
2021-02-19 16:45:04 +0100

    particles added

--- js/main.js
+++ js/main.js
@@ -15,6 +15,7 @@ function animate() {
     //ctx.fillRect(10, canvas.height - 90, 50, 50);
     bird.update();
     bird.draw();
+    handleParticles();
     requestAnimationFrame(animate);
     angle+=0.12;
 }
