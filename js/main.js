59efb70639ac2530ccf2da8c9be79b9ed639870f
Ailie McCorkindale <ailiem134@hotmail.com>
2021-02-26 15:08:10 +0100

    collision detection and game over

--- js/main.js
+++ js/main.js
@@ -16,6 +16,12 @@ function animate() {
     handleObstacles();
     bird.update();
     bird.draw();
+    ctx.fillStyle = "red";
+    ctx.font = "90px Georgia";
+    ctx.strokeText(score, 450, 70);
+    ctx.fillText(score, 450, 70);
+    handleCollisions();
+    if(handleCollisions()) return;
     handleParticles();
     requestAnimationFrame(animate);
     angle+=0.12;
@@ -32,3 +38,23 @@ window.addEventListener("keydown", function(e) {
 window.addEventListener("keyup", function(e) {
     if(e.code === "Space") spacePressed = false;
 });
+
+const bang = new Image();
+bang.src = 'bang.png';
+
+function handleCollisions() {
+    for(let i = 0; i < obstaclesArray.length; i++) {
+        if(bird.x < obstaclesArray[i].x + obstaclesArray[i].width &&
+            bird.x + bird.width > obstaclesArray[i].x &&
+            ((bird.y < 0 + obstaclesArray[i].top && bird.y + bird.height > 0) ||
+            (bird.y > canvas.height - obstaclesArray[i].bottom && bird.y + bird.height < canvas.height))
+            ) {
+                // collision detected
+                ctx.drawImage(bang, bird.x, bird.y, 50, 50);
+                ctx.font = "25px Georgia";
+                ctx.fillStyle = "black";
+                ctx.fillText(`Game Over, your score is ${score}`, 160, canvas.height/2 -10);
+                return true;
+            }
+    }
+}
